dc279b038005e3afbf831cd5e927c59ac22a27e8
Kmeade3 <kwmeade3@gmail.com>
2021-03-24 13:19:44 -0500

    Initial Commit

---
+++ Caps Network App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Caps Network App
+//
+//  Created by Kenneth W. Meade on 3/24/21.
+//  Copyright © 2021 Kenneth W. Meade. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Caps Network AppTests/Caps_Network_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Caps_Network_AppTests.swift
+//  Caps Network AppTests
+//
+//  Created by Kenneth W. Meade on 3/24/21.
+//  Copyright © 2021 Kenneth W. Meade. All rights reserved.
+//
+
+import XCTest
+@testable import Caps_Network_App
+
+class Caps_Network_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
